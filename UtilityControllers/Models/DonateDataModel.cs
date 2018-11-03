3bd377fe6d03642eafc1f42c16395badc0794296
dulyasit <dulyasit@hotmail.com>
2018-11-03 20:50:41 +0700

    แก้ไข Model เพิ่มเติม เพราะ กำหนดไว้เหมือนกัน

--- UtilityControllers/Models/DonateDataModel.cs
+++ UtilityControllers/Models/DonateDataModel.cs
@@ -10,21 +10,20 @@ namespace UtilityControllers.Models
         public int DocumentRunno { get; set; }
         public string WriteAt { get; set; }
         public DateTime? DocumentDate { get; set; }
-        public int MemberRunno { get; set; }
-        public string MemberId { get; set; }
         public string DonateType { get; set; }
         public string DonateObjective { get; set; }
-        public int DonatorRunno { get; set; }
-        public string DonatorId { get; set; }
-        public Double DonateAmount { get; set; }
-        public string MemberName { get; set; }
-        public string MemberID { get; set; }
+        public int MemberRunno { get; set; }
+        public string MemberId { get; set; }        
+        public string MemberName { get; set; }        
         public DateTime? MemberBirthdate { get; set; }
         public string MemberAddress { get; set; }        
         public string MemberTelephone { get; set; }
+        public int PositionNo { get; set; }
         public string MemberPosition { get; set; }
-        public string DonatorName { get; set; }
-        public string DonatorID { get; set; }
+        public int DonatorRunno { get; set; }
+        public string DonatorId { get; set; }
+        public Double DonateAmount { get; set; }
+        public string DonatorName { get; set; }        
         public string DonatorRegisterNO { get; set; }
         public string DonatorTaxID { get; set; }
         public string DonatorAddress { get; set; }
